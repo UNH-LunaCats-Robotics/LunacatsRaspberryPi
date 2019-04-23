46148e2f552b7c6ec2f25254399be69af7b43840
cathysandstrom <cms@rlsand.com>
2019-04-23 11:57:44 -0700

    updated obstacle manager

--- lidarCode/GetLidarObstacles.py
+++ lidarCode/GetLidarObstacles.py
@@ -1,9 +1,16 @@
-import LinkedList.py
+import ObstacleManager.py
 
 obsHistory = LinkedList(None, None, None)
 
+curObstacle = None
+ObstacleHistory = ObstacleManager()
+
+#this is what interprets the data
+def onLidarDataRecieved(data): 
+    
+    
 if __name__ == "__main__":
     global obsHistory 
-    fakeHeights = {0,1,2,5,4,-2,-1,3}
+    fakeHeights = 
     
     for
--- lidarCode/ObstacleManager.py
+++ lidarCode/ObstacleManager.py
@@ -1,6 +1,55 @@
-from enum import Enum
+import LinkedList.py
 
-class DetectedType(Enum):
-    HILL = 1
-    HOLE = -1
-    NONE = 0
+class ObstacleManager(object):
+    def __init__(self):
+        self.obstacleList = LinkedList(None, None, None)
+        self.count = 0
+            
+    def insertObstacle(obs, alist):
+        if alist.obstacle.y > obs.y:
+            if alist.next == None:
+                alist.next = LinkedList(obs, alist, None)
+                self.count += 1
+            else:
+                insertObstacle(obs, alist.next)
+        else:
+            prevNext = alist.next
+            if alist.prev == None:
+                prevObs = alist.obstacle
+                alist.obstacle = obs
+                alist.next = LinkedList(prevObs, prevNext)
+                self.count += 1
+            else:
+                alist.prev.next = LinkedList(obs, alist.prev, alist)
+                self.count += 1
+                
+    def insertObstacle(obs):
+        if self.obstacleList.obstacle == None:
+            self.obstacleList.obstacle = obs
+            self.count += 1
+        else:
+            insertObstacle(obs, self.obstacleList)
+
+    def getPossibleObstacle(DetectedType, y, x, alist):
+        if alist == None:
+            return None
+        if alist.obstacle == None:
+            return None
+
+        inBounds_X = (x >= alist.obstacle.x_start and x <= alist.obstacle.x_end)
+        inBounds_Y = (y >= alist.obstacle.y_start and y <= alist.obstacle.y_end)
+
+        if inBounds_X and inBounds_Y and DetectedType == alist.obstacle.type:
+            return alist.obstacle
+            
+        return isAlreadyObstacle(DetectedType, y, alist.next)
+
+    def getPossibleObstacle(DetectedType, y, x):
+        if self.count == 0:
+            return None
+        else: 
+            return isAlreadyObstacle(DetectedType, y, x, self.obstacleList)
+
+
+
+    
