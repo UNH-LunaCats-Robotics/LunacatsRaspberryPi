e2f1c374ad2db97a212a2c4e11eb39bdf1be485d
cathysandstrom <cms@rlsand.com>
2019-05-01 16:18:01 -0700

    Added Comments

--- Testing/PythonArduinoWireSignal.py
+++ Testing/PythonArduinoWireSignal.py
@@ -2,13 +2,36 @@ import gpiozero
 import enum
 from time import sleep
 
+'''
+#-----------------!!NOTICE!!-----------------#
+This code is not used and hasnt been tested.
+
+Original Problem: 
+    Serial Communication  on Arduino had a large 
+    delay when recieving serial input
+File's Purpose:
+    Communicate with arduino using pins as binary
+    inputs for driving for instantanious actions.
+Solution:
+    Serial Communication Delay was programmed into
+    the arduino. Changing the delay value allowed 
+    for instantious actions. 
+'''
+
 actionEnable = gpiozero.DigitalOutputDevice(31, False)
 action_q0 = gpiozero.DigitalOutputDevice(37, False)
 action_q1 = gpiozero.DigitalOutputDevice(35, False)
 action_q2 = gpiozero.DigitalOutputDevice(33, False)
 
 class Actions(enum.Enum):
-    
+    HALT =      0
+    FORWARD =   1
+    BACKWARD =  2
+    LEFT =      3
+    RIGHT =     4
+    ACTSTOP =   5
+    ACTUP =     6
+    ACTDOWN =   7
 
 def sendDecimalToArduino(num):
     setPinCorrectValue(((int)(num/4)     == 1), action_q2)
@@ -33,4 +56,4 @@ def setPinCorrectValue(status, pin):
 '''
 
 if __name__ == "__main__":
-    
+    pass
