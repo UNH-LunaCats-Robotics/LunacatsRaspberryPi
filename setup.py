c57a869e10e4454c2047c77353a7aae1bf5e751b
arbiter487 <arbiters487@gmail.com>
2019-05-05 18:03:36 -0400

    WIP

--- setup.py
+++ setup.py
@@ -2,29 +2,31 @@
 
 from distutils.core import setup, Extension
 
-pixy_module = Extension('_pixy',
-  include_dirs = ['/usr/include/libusb-1.0',
-  '/usr/local/include/libusb-1.0',
-  '../../../common/inc',
-  '../../../host/libpixyusb2/include/',
-  '../../../host/arduino/libraries/Pixy2'],
-  libraries = ['pthread',
-  'usb-1.0'],
-  sources =   ['pixy_wrap.cxx',
-  '../../../common/src/chirp.cpp',
-  '../../../host/libpixyusb2_examples/python_demos/pixy_python_interface.cpp',
-  '../../../host/libpixyusb2/src/usblink.cpp',
-  '../../../host/libpixyusb2/src/util.cpp',
-  '../../../host/libpixyusb2/src/libpixyusb2.cpp'])
+# The paths used in this file are relative to: PIXY_ROOT/build #
 
-import os
-print ("dir = ")
-print (os.path.dirname(os.path.realpath(__file__)))
+pixy_module = Extension('_pixy', 
+	include_dirs = ['/usr/include/libusb-1.0',
+  '/usr/local/include/libusb-1.0',
+	'../../src/common/inc',
+	'../../src/host/libpixyusb/src/utils',
+  '../../src/host/libpixyusb/include'],
+	libraries = ['boost_thread',
+	'boost_system',
+	'boost_chrono',
+	'pthread',
+	'usb-1.0'],
+	sources=['pixy_wrap.cxx', 
+	'../../src/common/src/chirp.cpp',
+	'../../src/host/libpixyusb/src/pixy.cpp',
+	'../../src/host/libpixyusb/src/chirpreceiver.cpp',
+	'../../src/host/libpixyusb/src/pixyinterpreter.cpp',
+	'../../src/host/libpixyusb/src/usblink.cpp',
+	'../../src/host/libpixyusb/src/utils/timer.cpp'])
 
 setup (name = 'pixy',
-  version = '0.1',
-  author = 'Charmed Labs, LLC',
-  description = """libpixyusb2 module""",
-  ext_modules = [pixy_module],
-  py_modules = ["pixy"],
-  )
+	version = '0.3',
+	author  = 'Charmed Labs, LLC',
+	description = """libpixyusb module""",
+	ext_modules = [pixy_module],
+	py_modules = ["pixy"],
+	)
