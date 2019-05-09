4f163cf09f0b87268e116e3d2eacee7b4fc536a7
cathysandstrom <cms@rlsand.com>
2019-05-09 12:39:08 -0700

    updated lidar experimental values code

--- Testing/LidarTesting/getAverageLidarValue.py
+++ Testing/LidarTesting/getAverageLidarValue.py
@@ -23,6 +23,7 @@ GPIO  6 = pin 31
 from lidar_lite import Lidar_Lite
 from time import sleep
 from gpiozero import DigitalOutputDevice 
+from __future__ import division
 
 lidarController = [DigitalOutputDevice(26), 
                    DigitalOutputDevice(19),
@@ -38,11 +39,12 @@ connected = lidar.connect(1)
 if connected < -1:
   print("Not Connected")
 
-loopedVal = 1
-
 if __name__ == "__main__":
     print("Starting Test")
-    while True:
+
+    count = 1000
+    c = 0
+    while c != count:
         for i in range(0,4):
             if i == 0:
                 lidarController[3].off()
@@ -51,21 +53,22 @@ if __name__ == "__main__":
             lidarController[i].on()
             print( str(i) + " " + str(lidarController[i].value) )
             
-            j = loopedVal
+            j = 0
             while j != 50:
-                #try:
-                dist = lidar.getDistance()
-                avg = avgValues[i]
-                print("Distance: "+ str(i) + " " + str(dist))
-                avgValues[i] = (avg+dist)/(j+loopedVal)
-                print("Average Value for "+ str(i) + " " + str(avgValues[i]))
-                sleep(0.01)
-                '''except:
+                try:
+                    dist = lidar.getDistance()
+                    avg = avgValues[i]
+                    print("Distance: "+ str(i) + " " + str(dist))
+                    avgValues[i] = avg+dist
+                    #print("Average Value for "+ str(i) + " " + str(avgValues[i]))
+                    sleep(0.01)
+                except:
                     #note that this error will cause problems for the average
                     print("\tRemote I/O Error Occur, Check connection")
-                    sleep(0.01)'''
+                    sleep(0.01)
                 j += 1
+        c += 1
 
-        for i in range(4):
-            print("Average Value for Lidar "+srt(i)+": "+str(avgValues[i]))
-        loopedVal += 50
+    for i in range(4):
+        print("Average Value for Lidar "+str(i)+": "+str(double(avgValues[i]/(count*50)))
+    #loopedVal += 50
