14df4dd3daa0609052e8f9651c6a9a19d57ae772
cathysandstrom <cms@rlsand.com>
2019-08-08 17:30:48 -0700

    added CMake

---
+++ 2019-2020/main.cpp
@@ -0,0 +1,23 @@
+#include "Headers/ArduinoSerial.h"
+
+#define BUF_SIZE 1024
+
+
+int main() {
+    try {
+	    ArduinoSerial serial(ttyACM0);
+
+        unsigned char cmd[] = "Lets test this messenger!";
+	
+        /* Whole response*/
+        char response[BUF_SIZE];
+        memset(response, '\0', sizeof response);
+        
+        serial.writeString(cmd);
+        serial.readString(response, BUF_SIZE);
+    } catch( invalid_argument e ) {
+        printf("Error: %s", e.what());
+    }
+
+	return 0;
+}
