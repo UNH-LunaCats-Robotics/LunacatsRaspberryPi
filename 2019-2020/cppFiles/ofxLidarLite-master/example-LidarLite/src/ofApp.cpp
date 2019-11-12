dfd994f0b3fba3709904033019e4b56c67cc9480
Shiqian Xi <sx1008@wildcats.unh.edu>
2019-11-12 13:55:24 -0500

    LidarLite1stChange

--- 2019-2020/cppFiles/ofxLidarLite-master/example-LidarLite/src/ofApp.cpp
+++ 2019-2020/cppFiles/ofxLidarLite-master/example-LidarLite/src/ofApp.cpp
@@ -2,42 +2,47 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	cout << "Here is the start of file" << endl;
 	myLidarLite = LidarLite();
 	myLidarLite.begin();
-	
+
 	// Exit if the lidar lite didn't initialize properly
 	if (!myLidarLite.hasBegun()) ofApp::exit();
-	
+
 	// Print the hardware version of the Lidar Lite
 	cout << "LIDAR Lite hardware version: " << myLidarLite.hardwareVersion() << endl;
 	cout << "LIDAR Lite software version: " << myLidarLite.softwareVersion() << endl;
-	
+
 	wDistance = -1;
+	myLidarLite.start();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	int desread = myLidarLite.startDistanceRead();
+	if(desread) {
+		cout << "myLidarLite read something" << endl;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 	ofSleepMillis(100);
-	
-	cout << "Frame rate = " << std::fixed << std::setw( 5 ) << std::setprecision( 2 ) 
+
+	cout << "Frame rate = " << std::fixed << std::setw( 5 ) << std::setprecision( 2 )
           << std::setfill( '0' ) <<ofGetFrameRate() << " Hz, ";
-		
+
 	// Read the distance
 	int distance = myLidarLite.distance();
 	cout << "Distance = " << distance << " cm, ";
-	
+
 	// Read the status (useful for debug)
 	//int status = myLidarLite.status();
 	//cout << myLidarLite.statusString(status);
-	
-	// Power user technique: 
+
+	// Power user technique:
 	// Weighting the new distance value by the measured signal strength
-	// Helps eliminate noise created by the sun and by not detecting any objects 
+	// Helps eliminate noise created by the sun and by not detecting any objects
 	int signalStrength = myLidarLite.signalStrength();
 	int minSigStrength = 20; // signal strength minimum
 	int fullSigStrength = 80; // signal strength that gets full weight
@@ -54,7 +59,7 @@ void ofApp::draw(){
 	}
 	cout << "wDistance = " << wDistance << " cm, ";
 	cout << "signalStrength = " << signalStrength << ", ";
-	
+
 	cout << endl;
 }
 
@@ -99,6 +104,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
---
+++ 2019-2020/lidarTest/getLidarValues.py
@@ -0,0 +1,86 @@
+'''
+#-----------------!!NOTICE!!-----------------#
+YOU NEED CAPACITORS TO POWER THE LIDAR
+
+This is really important since you can do 
+some real damage to the devine your testing with
+because it does not consistantly draw power.
+
+Capacitance: 500 <= uC <= 1000
+Capacitanc on Website: 680uC
+
+Wires Used: Red, Black, Green, Blue
+
+----SETUP----
+
+SINGLE CAPACITOR:
+The 5v
+
+Assume . is an unused pin hole.
++  is positive current input
+-  is negative current input
+X  is capacitor
+L  is lidar
+IN is input power
+
+IN+ . . . . . X+ . . . . . L+
+IN- . . . . . X- . . . . . L-
+
+CAPACITORS IN PARALLEL:
+
+Assume . is an unused pin hole.
++  is positive current input
+-  is negative current input
+X  is capacitor
+L  is lidar
+IN is input power
+W# is wire connections (W1+ -> W1+)
+
+IN+ . . . . . . . W2+ . . . . L+
+IN- . . . . . . . W2- . . . . L-
+----------------------------------
+.  W1+ .  .  .  W2+ .  .  .  .  .
+.  .   .  .  .  W1+ .  .  .  .  . 
+.  X+  .  .  .  X+  .  .  .  .  . 
+.  X-  .  .  .  X-  .  .  .  .  . 
+.  .   .  .  .  W2- .  .  .  .  . 
+.  W1- .  .  .  W1- .  .  .  .  .
+
+Connect positive leads for capacitors to eachother
+Connect negative leads for capacitors to eachother
+Power goes into the first capacitor's positive lead
+    and fed back from the last capacitor's positive lead
+Ground goes into the first capacitor's negative lead
+    and fed back from the last capacitor's negative lead
+    
+RASPBERRY PI WIRING
+
+The odd pin numbers are the top row starting from
+the right side closes to the power and increases to
+the left with the farthest pin being closest to the
+USB ports. Here is the Pinout:
+
+Red: 6   Black: 4    Blue: 3    Green: 5
+
+use sudo i2cdetect -y 1 to verify if it is connected
+correctly. 
+
+#--------------------------------------------#
+'''
+
+lidar = Lidar_Lite()
+connected = lidar.connect(1)
+
+if connected < -1:
+  print("Not Connected")
+
+if __name__ == "__main__":
+    print("Starting Test")
+    while True:
+        try:
+            
+            print "Distance: "+lidar.getDistance()
+            sleep(0.1)
+        except:
+            print "\tRemote I/O Error Occur, Check connection"
+            sleep(0.1)
