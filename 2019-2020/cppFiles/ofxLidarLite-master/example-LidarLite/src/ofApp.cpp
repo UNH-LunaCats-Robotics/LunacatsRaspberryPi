998f12984d6ff8ee47c1c3abc2b3295c7d847322
Shiqian Xi <sx1008@wildcats.unh.edu>
2019-11-18 14:51:24 -0500

    Update ofApp.cpp

--- 2019-2020/cppFiles/ofxLidarLite-master/example-LidarLite/src/ofApp.cpp
+++ 2019-2020/cppFiles/ofxLidarLite-master/example-LidarLite/src/ofApp.cpp
@@ -72,6 +72,7 @@ double ofApp::getXlength(double angle, double straightDistance){
 	double xlength = sinangle * straightDistance;
 	return xlength;
 }
+
 // straightDistance is the distance from the lidar to the rock
 // HereY is the Y of the current position
 // UR means up right
@@ -112,7 +113,7 @@ double ofApp::getYforUR(double angle, double straightDistance, double hereY){
 double ofApp::getXforUR(double angle, double straightDistance, double hereX){
 	double pi = 3.14159265;
 	double result, cosnum;
-	if(angle == 0 || angle == 360){
+	if(angle == 0 || angle == 360 || ngle == 180){
 		result = hereX;
 	}
 	else if(angle < 90 && angle > 0){
@@ -130,12 +131,12 @@ double ofApp::getXforUR(double angle, double straightDistance, double hereX){
 		angle = angle - 270;
 		cosnum = sin(angle * pi /180);
 		result = cosnum * straightDistance;
-		result = result - hereY;
+		result = hereX - result;
 	}
 	else if(angle > 90 && angle < 180){
 		cosnum = sin(angle * pi /180);
 		result = cosnum * straightDistance;
-		result = result + hereX;
+		result = result - hereX;
 	}
 	else{
 		result = -1;
@@ -143,6 +144,77 @@ double ofApp::getXforUR(double angle, double straightDistance, double hereX){
 	return result;
 }
 
+// straightDistance is the distance from the lidar to the rock
+// HereY is the Y of the current position
+// UL means up Left
+double ofApp::getXforUL(double angle, double straightDistance, double hereX){
+	double pi = 3.14159265;
+	double result, cosnum;
+	if(angle == 0 || angle == 360 || angle == 180){
+		result = hereX;
+	}
+	else if(angle < 90 && angle > 0){
+		cosnum = sin(angle * pi /180);
+		result = cosnum * straightDistance;
+		result = result + hereX;
+	}
+	else if(angle == 90){
+		result = hereX + straightDistance;
+	}
+	else if(angle == 270){
+		result = hereX - straightDistance;
+	}
+	else if(angle > 270 && angle < 360){
+		angle = angle - 270;
+		cosnum = sin(angle * pi /180);
+		result = cosnum * straightDistance;
+		result = hereX - result;
+	}
+	else if(angle > 180 && angle < 270){
+		angle = angle - 180;
+		cosnum = sin(angle * pi /180);
+		result = cosnum * straightDistance;
+		result = hereX - result;
+	}
+	else{
+		result = -1;
+	}
+	return result;
+}
+
+// straightDistance is the distance from the lidar to the rock
+// HereY is the Y of the current position
+// UL means up Left
+double ofApp::getYforUL(double angle, double straightDistance, double hereY){
+	double pi = 3.14159265;
+	double result, cosnum;
+	if(angle == 0 || angle == 360){
+		result = straightDistance + hereY;
+	}
+	else if(angle < 90 && angle > 0){
+		cosnum = cos(angle * pi /180);
+		result = cosnum * straightDistance;
+		result = result + hereY;
+	}
+	else if(angle == 90 || angle == 270){
+		result = hereY;
+	}
+	else if(angle > 270 && angle < 360){
+		angle = angle - 270;
+		cosnum = cos(angle * pi /180);
+		result = cosnum * straightDistance;
+		result = result + hereY;
+	}
+	else if(angle > 180 && angle < 270){
+		cosnum = cos(angle * pi /180);
+		result = cosnum * straightDistance;
+		result = hereY - result;
+	}
+	else{
+		result = -1;
+	}
+	return result;
+}
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
